e8ba881f2d7ef83b8daf7a12332e4d16674f09da
Eduardo Amroginski Santos <eduardo.amroginski@pampa.compasso>
2021-04-21 19:00:45 -0300

    NLW#5 Dia 3 - Criando tabelas Users, Messages e criando o relacionamento entre elas

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,8 @@
-import { request, response, Router } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsController } from "./controllers/SettingsController";
+import { Router } from "express";
+import { MessagesController } from "./controllers/MessagesController";
 
-import { SettingsRepository } from "./repositories/SettingsRepository";
+import { SettingsController } from "./controllers/SettingsController";
+import { UsersController } from "./controllers/UsersController";
 
 const routes = Router();
 
@@ -16,8 +16,15 @@ const routes = Router();
  * --- Exemplo: {"name": "Fulano de tal" }
  */
 
- const settingsController = new SettingsController();
+const settingsController = new SettingsController();
+const usersController = new UsersController();
+const messagesController = new MessagesController();
 
 routes.post("/settings", settingsController.create);
+
+routes.post("/users", usersController.create);
+
+routes.post("/messages", messagesController.create);
+routes.get("/messages/:id", messagesController.showByUser);
 
 export { routes };
